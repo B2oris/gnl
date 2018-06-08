b85a4485d290d9b4b6eabd589237b61afcdd4c99
b2oris <skryllex78@gmail.com>
2018-06-08 15:22:20 +0200

    juste .c

--- get_next_line.c
+++ get_next_line.c
@@ -10,7 +10,90 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int		get_next_line(const int fd, char **line)
+#include "get_next_line.h"
+
+#include "get_next_line.h"
+
+static int	last_line(char *buff)
 {
-	
+	int		i;
+
+	i = 0;
+	while (buff[i] != '\n' && buff[i])
+		i++;
+	if (buff[i] == '\n')
+	{
+		buff[i] = '\0';
+		return (i);
+	}
+	else
+		return (-1);
+}
+
+static char	*join(char *buff, char *tab)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	if (buff)
+		i = ft_strlen(buff);
+	if (tab)
+		j = ft_strlen(tab);
+	str = (char *)malloc(sizeof(*str) * (i + j + 1));
+	ft_memcpy(str, buff, i);
+	ft_memcpy(str + i, tab, j);
+	str[i + j] = '\0';
+	free(buff);
+	ft_bzero(tab, BUFF_SIZE + 1);
+	return (str);
+}
+
+static int	verif(char **buff, char **tab, char **line)
+{
+	char	*str;
+	int		last;
+
+	*buff = join(*buff, *tab);
+	last = last_line(*buff);
+	if (last > -1)
+	{
+		*line = ft_strdup(*buff);
+		str = *buff;
+		*buff = ft_strdup(*buff + last + 1);
+		free(str);
+		return (1);
+	}
+	return (0);
+}
+
+int			get_next_line(int const fd, char **line)
+{
+	static char *buff[BUFF_SIZE];
+	char		*tmp;
+	int			ret;
+	int			result;
+
+	tmp = ft_strnew(BUFF_SIZE);
+	if (!line || BUFF_SIZE <= 0 || fd < 0 || (ret = read(fd, tmp, 0)) < 0)
+		return (-1);
+	while ((ret = read(fd, tmp, BUFF_SIZE)) > 0)
+	{
+		result = verif(&buff[fd], &tmp, line);
+		free(tmp);
+		if (result == 1)
+			return (1);
+		tmp = ft_strnew(BUFF_SIZE);
+	}
+	if ((result = verif(&buff[fd], &tmp, line)))
+		return (1);
+	else if (ft_strlen(buff[fd]) > 0)
+	{
+		*line = ft_strdup(buff[fd]);
+		ft_strdel(&buff[fd]);
+		return (1);
+	}
+	return (result);
 }
